f776c9ade8c96264fa50c99ebe11f606eb2991c7
Марат Шахмоведев <m.shakh-work@mail.ru>
2023-02-26 14:15:34 +0300

    auth v3

--- src/components/header/top_of_header/Top.tsx
+++ src/components/header/top_of_header/Top.tsx
@@ -8,46 +8,48 @@ import {authService} from './../../../store/Auth';
 import {NavLink, useNavigate} from "react-router-dom";
 import {observer} from "mobx-react";
 
-const Top = (props: any) => {
-
-    const unAuthHandler = () => {
-        authService.setAuth(false);
-    }
-
-    return <div className={classes.top}>
-        <form action="#" method="get">
-            <div className={classes.search}>
-                <img className={classes.search__icon}
-                     alt={"Иконка поиска"}
-                     src={searchicon}/>
-                <input className={classes.search__form} type="text" placeholder="Поиск"/>
-            </div>
-        </form>
-        <div className={classes.logo}>
-            <a href="#">
-                <img
-                    alt={"Логотип"}
-                    src={logo}/>
-            </a>
-        </div>
-        <div className={classes.languages}>
-            <span>RU |</span> UA
-        </div>
-        {authService.getAuth() &&
-            <div className={classes.loginName}>
-                {authService.getLogin()}
-            </div>
+const Top = observer((props: any) => {
+        const navigate = useNavigate();
+        const unAuthHandler = () => {
+            navigate('/');
+            authService.setAuth(false);
+            authService.setRole(null);
+            authService.setLogin(null);
         }
-        <div className={classes.icons}>
-            <img className={classes.cart}
-                 alt={"Иконка корзины"}
-                 src={cart}/>
-            <NavLink onClick={unAuthHandler} to={"/"}>
-                <img className={classes.exit}
+
+        return <div className={classes.top}>
+            <form action="#" method="get">
+                <div className={classes.search}>
+                    <img className={classes.search__icon}
+                         alt={"Иконка поиска"}
+                         src={searchicon}/>
+                    <input className={classes.search__form} type="text" placeholder="Поиск"/>
+                </div>
+            </form>
+            <div className={classes.logo}>
+                <a href="#">
+                    <img
+                        alt={"Логотип"}
+                        src={logo}/>
+                </a>
+            </div>
+            <div className={classes.languages}>
+                <span>RU |</span> UA
+            </div>
+            {authService.getAuth() &&
+                <div className={classes.loginName}>
+                    {authService.getLogin()}
+                </div>
+            }
+            <div className={classes.icons}>
+                <img className={classes.cart}
+                     alt={"Иконка корзины"}
+                     src={cart}/>
+                <img onClick={unAuthHandler} className={classes.exit}
                      alt={"Иконка выхода"}
                      src={exit}/>
-            </NavLink>
+            </div>
         </div>
-    </div>
-}
+    }
+)
 export default Top;
